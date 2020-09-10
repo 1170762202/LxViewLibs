ed3db0a8eef9b40c46af0cc4a067688c5a0864f5
1170762202@qq.com <kiss520>
2020-09-10 14:00:23 +0800

    模仿boss直聘我的页面

---
+++ app/src/main/java/com/zlx/lxviewlibs/ui/coordinator/BossAc.java
@@ -0,0 +1,17 @@
+package com.zlx.lxviewlibs.ui.coordinator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zlx.lxviewlibs.R;
+
+
+public class BossAc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ac_boss);
+    }
+}
